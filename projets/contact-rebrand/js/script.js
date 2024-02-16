f5077f6bbd796475f66963d2bfd05875ac8cbc84
Valentin de Brabandère <valentindebrabandere@gmail.com>
2024-02-16 06:14:12 +0100

    oldlogos

--- projets/contact-rebrand/js/script.js
+++ projets/contact-rebrand/js/script.js
@@ -9,20 +9,17 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-console.log("test")
+const video = document.querySelector('.js-header__video');
 
-// const video = document.querySelector('.js-header__video');
+// Track the scroll position
+window.addEventListener('scroll', () => {
+    // Get the distance from the top of the viewport to the top of the video
+    const videoTop = video.getBoundingClientRect().top;
 
-// // Track the scroll position
-// window.addEventListener('scroll', () => {
-//     // Get the distance from the top of the viewport to the top of the video
-//     const videoTop = video.getBoundingClientRect().top;
-//     console.log(videoTop)
-
-//     // If the video is outside the viewport by at least 500px, decrease the volume
-//     if (videoTop < -500 || videoTop > window.innerHeight) {
-//         video.volume = 0.2; // Adjust the volume as needed
-//     } else {
-//         video.volume = 1; // Reset the volume
-//     }
-// });
+    // If the video is outside the viewport by at least 500px, decrease the volume
+    if (videoTop < -500 || videoTop > window.innerHeight) {
+        video.volume = 0.2; // Adjust the volume as needed
+    } else {
+        video.volume = 1; // Reset the volume
+    }
+});
